c14157d374df8b2e6ff9f6e19e1a022246643bf1
Lowan Lebioda-Leger <llebioda@student.42.fr>
2025-08-18 15:53:54 +0200

    reworked downloader to be generic

--- dataset_downloader.py
+++ dataset_downloader.py
@@ -5,35 +5,53 @@ from datasets import load_dataset, IterableDataset
 OUTDIR = "datasets"
 
 
-def stream_dataset(dataset: IterableDataset, limit: int) -> Generator[dict, Any, None]:
+def stream_dataset(dataset: IterableDataset, limit: int) -> Generator[dict[str, str], Any, None]:
+    if limit < 0:
+        for example in dataset:
+            yield example
+        return
+
     for i, example in enumerate(dataset):
         if i > limit:
             break
         yield example
 
 
-def download_CommonCrawl_french() -> None:
-    # Load the French subset using streaming
+def download_dataset(
+    name: str,
+    path: str,
+    dataset_name: str,
+    column: str = 'text',
+    limit: int = -1
+) -> None:
     dataset: IterableDataset = load_dataset(
-        "BramVanroy/CommonCrawl-CreativeCommons",
-        name='CC-MAIN-2019-30-fra',
+        path,
+        name=dataset_name,
         split='train',
         streaming=True,
     )
 
-    with open(os.path.join(OUTDIR, "CommonCrawl_french.txt"), 'w', encoding='utf-8') as f:
-        try:
-            for example in stream_dataset(dataset, 100):
-                text: str = example.get('text', "")
-                text = text.strip().replace('\n', "\\n")
+    try:
+        if column not in dataset.column_names:
+            raise KeyError(f"The dataset doesn't have a column named '{column}'")
+
+        with open(os.path.join(OUTDIR, name + ".txt"), 'w', encoding='utf-8') as f:
+            for example in stream_dataset(dataset, limit):
+                text = example[column].strip().replace('\n', "\\n")
                 if text:  # Only write non-empty text
                     f.write(text + '\n')
 
-        except Exception as e:
-            print(f"CommonCrawl_french : Error : {e}")
-        finally:
-            gc.collect()  # DONT REMOVE, else python segfault
+    except Exception as e:
+        print(f"{name} : {type(e).__name__} : {e}")
+    finally:
+        gc.collect()  # DON'T REMOVE, else python segfault
 
 
 if __name__ == "__main__":
-    download_CommonCrawl_french()
+    # download_dataset(
+    #     "CommonCrawl_french",
+    #     "BramVanroy/CommonCrawl-CreativeCommons",
+    #     "CC-MAIN-2019-30-fra",
+    #     limit=100
+    # )
+    download_dataset("sib200_french", "mteb/sib200", "fra_Latn", limit=100)
